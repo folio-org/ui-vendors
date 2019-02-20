d2b6dc0c55574510c042cfe0035c200e6f617a32
arvind <bonandrion@gmail.com>
2019-02-20 16:37:23 -0700

    UIV-76 Append vendor category in address, phone number, emails and urls

--- src/ContactInformation/ContactInformationView.js
+++ src/ContactInformation/ContactInformationView.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+
 import { Row, Icon } from '@folio/stripes/components';
 import { AddressInfoView, PhoneNumbersView, EmailView, UrlsView } from './ContactInfoViewGroup';
 
@@ -10,6 +11,13 @@ class ContactInformationView extends Component {
       dropdown: PropTypes.object.isRequired
     })
   };
+
+  getVendorcategory() {
+    const { parentResources } = this.props;
+    const data = ((parentResources || {}).vendorCategory || {}).records || [];
+    if (!data || data.length === 0) return null;
+    return data;
+  }
 
   render() {
     const { initialValues } = this.props;
@@ -23,10 +31,10 @@ class ContactInformationView extends Component {
       <div style={{ width: '100%' }}>
         <Row>
           <div style={{ width: '100%' }}>
-            <AddressInfoView dataVal={initialValues.addresses} />
-            <PhoneNumbersView dataVal={initialValues.phone_numbers} />
-            <EmailView dataVal={initialValues.emails} />
-            <UrlsView dataVal={initialValues.urls} />
+            <AddressInfoView dataVal={initialValues.addresses} dropdownVendorCategories={this.getVendorcategory()} />
+            <PhoneNumbersView dataVal={initialValues.phone_numbers} dropdownVendorCategories={this.getVendorcategory()} />
+            <EmailView dataVal={initialValues.emails} dropdownVendorCategories={this.getVendorcategory()} />
+            <UrlsView dataVal={initialValues.urls} dropdownVendorCategories={this.getVendorcategory()} />
           </div>
         </Row>
       </div>
